a21b1b88aedc72b00ccfc1315903146c1069e012
Lilysaria <bloomyday56@gmail.com>
2024-04-07 20:39:49 -0700

    Add models and functionalities for practicing django -add finch, feeding, and toy model -add routes, admin, and base template for navigation -add functionality using CBVs and FBVs

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -9,3 +9,34 @@ class Finch(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)   
+    # Add new Feeding model below Cat model
+# One Cat has many Feedings, A feeding belongs to a cat
+class Feeding(models.Model):
+    date = models.DateField("feeding date")
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0],
+    )
+
+    # create a cat_id Foreign Key in psql
+    # we don't put the id, django does automatically
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+      # change the default sort
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 # Add this cats list below the imports
 
 # Create your views here.
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = [ 'species', 'description', 'migration_patterns', 'habitat']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
 def home(request):
     return render(request, 'home.html')
 
@@ -23,9 +37,22 @@ def finches_index(request):
 # cat_id comes from the path in the urls.py 
 # path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
 def finches_detail(request, finch_id):
-	# tell the model to find the row that matches cat_id from the request in the database
-	finch = Finch.objects.get(id=finch_id)
-	return render(request, 'finches/detail.html', {
-		'finch': finch
-		# cat (the key) is the variable name in cats/detail.html 
-	})
+  finch = Finch.objects.get(id=finch_id)
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+    # include the cat and feeding_form in the context
+    'finch': finch, 'feeding_form': feeding_form
+  })
+
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
